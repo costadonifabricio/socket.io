fffa3ac1af0c44928e1c81ca1bbbccb7d70e7a52
Fabian <fabianvillagra28@gmail.com>
2023-11-06 15:58:56 -0300

    feat: carpeta js

---
+++ server/public/js/socket.js
@@ -0,0 +1,20 @@
+let socket = io();
+
+let messages = document.getElementById('messages');
+let form = document.getElementById('form');
+let input = document.getElementById('input');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
