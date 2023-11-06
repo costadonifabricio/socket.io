9f3be111d9ae0e85958673ca479416e8ff9ffa65
Fabriciovich <fabriciocostadoni@gmail.com>
2023-11-06 16:43:38 -0300

    Se agrego array vacio para cargar los mensajes en localstorage

--- server/public/js/socket.js
+++ server/public/js/socket.js
@@ -1,20 +1,38 @@
 let socket = io();
-    
 let messages = document.getElementById('messages');
 let form = document.getElementById('form');
 let input = document.getElementById('input');
+let messageHistory = JSON.parse(localStorage.getItem('messageHistory')) || [];
 
-form.addEventListener('submit', function(e) {
+// Mostrar mensajes almacenados al cargar la página
+messageHistory.forEach(function (msg) {
+  let item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+});
+
+form.addEventListener('submit', function (e) {
   e.preventDefault();
   if (input.value) {
+    // Enviar mensaje al servidor a través del socket
     socket.emit('chat message', input.value);
+
     input.value = '';
   }
 });
 
-socket.on('chat message', function(msg) {
+// Escuchar mensajes del servidor
+socket.on('chat message', function (msg) {
   let item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
+
+  // Almacenar mensaje en el historial
+  messageHistory.push(msg);
+  localStorage.setItem('messageHistory', JSON.stringify(messageHistory));
+
   window.scrollTo(0, document.body.scrollHeight);
 });
+
+
+
